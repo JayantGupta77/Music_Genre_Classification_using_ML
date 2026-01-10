cd277c0298adab1e3fbb2d3447ae3a20b101046e
JayantGupta77 <gjayant517@gmail.com>
2026-01-10 20:05:10 +0530

    ml

--- check_encoder.py
+++ check_encoder.py
@@ -31,6 +31,7 @@ try:
     except Exception:
         scaler = None
 
+
     # reduce to columns model expects if necessary
     # try predicting (may raise if columns mismatch)
     preds = model.predict(X)
